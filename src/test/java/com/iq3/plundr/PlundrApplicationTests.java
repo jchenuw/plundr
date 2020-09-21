d5bdbdfd4df03466e7b06424baad4de1da55e6b8
Jier <cje9507@gmail.com>
2020-09-20 20:03:20 -0400

    Initial commit

---
+++ src/test/java/com/iq3/plundr/PlundrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iq3.plundr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlundrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
