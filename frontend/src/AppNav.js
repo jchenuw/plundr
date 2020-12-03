591db4054b133990bea6bb82231a0ddb10b6615c
Jier <cje9507@gmail.com>
2020-12-03 05:52:28 -0500

    Added base application navigation bar using react-strap

--- frontend/src/AppNav.js
+++ frontend/src/AppNav.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 class AppNav extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isOpen: false };
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
     render() {
-        return <Navbar color="dark" dark expand="md">
-            <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
-        </Navbar>
+        return (
+            <div>
+                <Navbar className="navbar" color="dark" dark expand="md">
+                    <NavbarBrand>Plundr</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink tag={Link} to="/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    href="https://github.com/jchenuw">
+                                    Author
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
     }
 }
 
