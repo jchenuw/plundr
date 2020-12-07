2c1a06e878e9da9f1366377f3346fb2a6d18a578
Jier <cje9507@gmail.com>
2020-12-07 04:25:34 -0500

    Create base custom User Principal

---
+++ src/main/java/com/iq3/plundr/security/UserPrincipalImpl.java
@@ -0,0 +1,58 @@
+package com.iq3.plundr.security;
+
+import com.iq3.plundr.model.User;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class UserPrincipalImpl implements UserDetails {
+
+	private User user;
+
+	public UserPrincipalImpl(User user) {
+		this.user = user;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+
+	public User getUser() {
+		return user;
+	}
+}
