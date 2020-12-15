a487e0a6e574a69e9140f357d5fc91468249a499
Jier <cje9507@gmail.com>
2020-12-15 03:04:30 -0500

    Added findByAccountNumber to AccountRepository

--- src/main/java/com/iq3/plundr/repository/AccountRepository.java
+++ src/main/java/com/iq3/plundr/repository/AccountRepository.java
@@ -4,4 +4,7 @@ import com.iq3.plundr.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+	Account findByAccountNumber(int accountNumber);
+
 }
