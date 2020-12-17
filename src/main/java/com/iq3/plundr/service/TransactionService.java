07b2b6d1ea3f06d6212e4c82fe6a389242cc3941
Jier <cje9507@gmail.com>
2020-12-17 04:15:50 -0500

    Added and implemented basic TransactionService methods

--- src/main/java/com/iq3/plundr/service/TransactionService.java
+++ src/main/java/com/iq3/plundr/service/TransactionService.java
@@ -1,4 +1,17 @@
 package com.iq3.plundr.service;
 
+import com.iq3.plundr.model.Transaction;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TransactionService {
+
+	List<Transaction> findAll();
+
+	Optional<Transaction> findByTransactionId(Long id);
+
+	Transaction saveTransaction(Transaction transaction);
+
+	void deleteTransaction(Long id);
 }
--- src/main/java/com/iq3/plundr/service/TransactionServiceImpl.java
+++ src/main/java/com/iq3/plundr/service/TransactionServiceImpl.java
@@ -1,4 +1,36 @@
 package com.iq3.plundr.service;
 
+import com.iq3.plundr.model.Transaction;
+import com.iq3.plundr.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TransactionServiceImpl implements TransactionService{
+
+	@Autowired
+	private TransactionRepository transactionRepository;
+
+	@Override
+	public List<Transaction> findAll() {
+		return transactionRepository.findAll();
+	}
+
+	@Override
+	public Optional<Transaction> findByTransactionId(Long id) {
+		return transactionRepository.findById(id);
+	}
+
+	@Override
+	public Transaction saveTransaction(Transaction transaction) {
+		return transactionRepository.save(transaction);
+	}
+
+	@Override
+	public void deleteTransaction(Long id) {
+		transactionRepository.deleteById(id);
+	}
 }
