e67f13db3ef6e0f074626fc09c71e91a575554c2
Jier <cje9507@gmail.com>
2021-01-11 18:46:14 -0500

    Add finding all transactions from an Account by accountNumber

--- src/main/java/com/iq3/plundr/repository/TransactionRepository.java
+++ src/main/java/com/iq3/plundr/repository/TransactionRepository.java
@@ -1,7 +1,11 @@
 package com.iq3.plundr.repository;
 
+import com.iq3.plundr.model.Account;
 import com.iq3.plundr.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+	List<Transaction> findByAccountOwner(Account account);
 }
